0a4d1c2ced3673926f8aec1aab97436ab8d5f77b
Maroun <maroun-karam@hotmail.com>
2024-08-26 13:59:25 +0300

    .

--- server.js
+++ server.js
@@ -169,6 +169,23 @@ app.post("/remove-notification", async (req, res) => {
   }
 });
 
+app.post("/add-notification", async (req, res) => {
+  const { userId, notificationUserId } = req.body;
+
+  try {
+    const user = await Users.findById(userId);
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    user.notification_userId = notificationUserId;
+    await user.save();
+
+    res.status(200).json({ message: "Notification user ID added successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 //Home api : announcement + points + expired
 app.post("/home", async (req, res) => {
