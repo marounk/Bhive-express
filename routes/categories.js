d3fa3f6b087509563d60cfb5a5f7f748ceac7591
Maroun <maroun-karam@hotmail.com>
2024-09-21 18:07:42 +0300

    .

--- routes/categories.js
+++ routes/categories.js
@@ -127,7 +127,7 @@ router.patch("/:id", getCategory, async (req, res) => {
         if (req.body.name != null) {
           res.cat.name = req.body.name;
         }
-        if (req.body.icon != null) {
+        if (req.body.image != null) {
           res.cat.icon = req.body.icon;
         }
         if (req.body.color != null) {
